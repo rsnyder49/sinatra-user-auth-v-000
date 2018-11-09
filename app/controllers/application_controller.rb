fa254c173a78212358cdf5afcbc6e5cbf260f749
Robert Snyder <r.snyder49@gmail.com>
2018-11-09 00:12:25 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -29,7 +29,7 @@ class ApplicationController < Sinatra::Base
 
   post '/sessions' do
     @user = User.find_by(email: params[:email], password: params[:password])
-    if @user ! nil
+    if @user !nil
       session[:id] = @user.id
       redirect '/users/home'
     else
